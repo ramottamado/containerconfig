d7e3ca25824e8d5a3443cd3374c93124b2aef9bf
Tamado Sitohang <tamado@tamado.codes>
2025-12-27 22:55:47 +0700

    Update

--- hive/env.sh
+++ hive/env.sh
@@ -1,7 +1,8 @@
-S3_ENDPOINT_URL=http://s3.saoirse.home.arpa
-DEFAULT_FS=s3a://tamado
+S3_ENDPOINT_URL=https://s3.saoirse.home.arpa
+DEFAULT_FS=s3a://hive
 HIVE_SERVER2_THRIFT_PORT=10000
 HIVE_WAREHOUSE_PATH=/warehouse
 HADOOP_CLASSPATH=/opt/hadoop/share/hadoop/tools/lib/*
+HADOOP_OPTS=-Djavax.net.ssl.trustStore=/trustStore.jks -Djavax.net.ssl.trustStorePassword=changeit
 DB_DRIVER=postgres
 IS_RESUME=true
